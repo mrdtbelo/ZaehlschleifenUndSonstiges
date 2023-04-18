73b11d95478bb46afbd0f6818dec9a9369d99032
mrdtbelo <belomaridiat@gmail.com>
2023-04-18 10:40:04 +0200

    1

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,2 +1,12 @@
 public class MainProgram {
+
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("Wert von i: " + i);
+            if(i == 3){
+                System.out.println("Holdriho!");
+            }
+        }
+    }
 }
