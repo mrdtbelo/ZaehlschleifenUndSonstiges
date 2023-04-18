88a67aed4aca54d25d28c1afad4297fc0ec2d406
mrdt.07 <belomaridiat@gmail.com>
2023-04-18 19:39:52 +0200

    qwer

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -2,11 +2,27 @@ public class MainProgram {
 
     public static void main(String[] args) {
 
-        for(int i = 0; i < 5; i++){
+        int start;
+        int end;
+
+        start = 335;
+        end = 600;
+
+        for(int i = 0; i < 5; i++) {
             System.out.println("Wert von i: " + i);
-            if(i == 3){
+            if (i == 3) {
                 System.out.println("Holdriho!");
             }
         }
+        public static void printSomeNumbers(){
+            for(int i = 6; i <= 23; i++) {
+                System.out.println("Wert von i: " + i);
+            }
+        }}}
+        /*public static void printNumbers(){
+        for(int i = start; i < end; i++){
+            System.out.println("Wert von i: " + i);
+        }
     }
+}
 }
