7990edbcc0805f0c82049dabe12a8e59e1c9b63d
mrdtbelo <belomaridiat@gmail.com>
2023-04-25 11:20:46 +0200

    1

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,31 +1,47 @@
 public class MainProgram {
 
     public static void main(String[] args) {
+        rectangleStars(1,4);
+        System.out.println(powerOfTwo(3));
 
-        int start;
-        int end;
+        }
 
-        start = 335;
-        end = 600;
-
-        for(int i = 0; i < 5; i++) {
+    public static void printSomeNumbers() {
+        for (int i = 6; i <= 23; i++) {
             System.out.println("Wert von i: " + i);
-            if (i == 3) {
-                System.out.println("Holdriho!");
-            }
+
         }
-        /*public static void printSomeNumbers(){
-            for(int i = 6; i <= 23; i++) {
-                System.out.println("Wert von i: " + i);
-            }
-        }
-        public static void printNumbers(){
-        for(int i = start; i <= end; i++){
+    }
+    public static void printNumbers(int start, int end) {
+        for (int i = start; i <= end; i++) {
             System.out.println("Wert von i: " + i);
         }
     }
-        public static int sumUp(){
-            for(int i = start; i <= end; i+i){
-         */
+    public static int sumUp(int start, int end) {
+        int sum = 0;
+        for (int i = start; i <= end; i++) {
+            sum = sum + 1;
+        }
+        return sum;
+    }
+    public static int powerOfTwo(int p){
+        if(p < 0) return -1;
+        int powerOf2 = 1;
+        for (int i = 0; i<= p; i++ ) {
+            powerOf2 = powerOf2 * 2;
+        } return powerOf2;
+    }
+    public static int sumUpPowerOfTwo(int n){
+        int sum = 0;
+        for(int i = 0; i<= n; i++){
+            sum = sum + powerOfTwo(i);
+        } return sum;
+    }
+    public static void rectangleStars(int width, int height){
+        for(int i = 0; i <= width; i++){
+            for(int j = 0; j <= height; j++) {
+                //gSystem.out.print();
+            }
+        }
     }
 }
